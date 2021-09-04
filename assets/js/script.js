18c36e523efd72f4f52a039e43d53ba7fc80e19a
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-09-03 22:16:26 -0400

    html/css complete - working on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,93 @@
 //DOM Selectors
 
+//Jumbotron Date
+var todaysDate = moment()
+$("#currentDay").text(todaysDate.format("dddd, MMMM Do YYYY"));
+
+//Hour containers
+var timeSlotContainer = $('#time-container')
+var nineAm = $('#9am');
+var tenAm = $('#10am');
+var elevenAm = $('#11am');
+var twelvePm = $('#12pm');
+var onePm = $('#1pm');
+var twoPm = $('#2pm');
+var threePm = $('#3pm');
+var fourPm = $('#4pm');
+var fivePm = $('#5pm');
+var currentIndex = 0;
+
+//Textbox
+var calendarTextBoxEntry = $('#calendar-event');
+
+//Submit buttons
+var allSubmitBtns = $('.submitBtn')
+var saveBtn1 = $('#save-btn1');
+var saveBtn2 = $('#save-btn2');
+var saveBtn1 = $('#save-btn3');
+var saveBtn1 = $('#save-btn4');
+var saveBtn1 = $('#save-btn5');
+var saveBtn1 = $('#save-btn6');
+var saveBtn1 = $('#save-btn7');
+var saveBtn1 = $('#save-btn8');
+var saveBtn1 = $('#save-btn9');
+
+//Current time
+const currentTime = moment().format("HHmm");
+console.log(currentTime);
+
+nineAm = 0900;
+tenAm = 1000;
+elevenAm = 1100;
+twelvePm = 1200;
+onePm = 1300;
+twoPm = 1400;
+threePm = 1500;
+fourPm = 1600;
+fivePm = 1700;
+
+
+saveBtn1.on('click', function () {
+    console.log(hi)
+})
+
+
+
+
+// if ($('#9am').value < currentTime) {
+//     $('#9am').addClass('past');
+// }
+
+
+
+// function colorCodedTime() {
+
+
+
+//     if (timeSlotContainer[currentIndex].value == currentTime) {
+//         timeSlotContainer[currentIndex].addClass('present')
+//     };
+
+
+//     if (timeSlotContainer[currentIndex].value < currentTime) {
+//         timeSlotContainer[currentIndex].addClass('past')
+//     };
+
+//     if (timeSlotContainer[currentIndex].value > currentTime) {
+//         timeSlotContainer[currentIndex].addClass('future')
+//     };
+
+// }
+
+// colorCodedTime();
+
+// allSubmitBtns.on('click', function () {
+//     // console.log(e);
+//     // e.preventDefault();
+//     localStorage.setItem('calendar-event', JSON.stringify(calendarTextBoxEntry));
+
+
+// });
 
 
 
@@ -18,8 +106,6 @@
 
 
 
-var now = moment()
-$("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 
 
